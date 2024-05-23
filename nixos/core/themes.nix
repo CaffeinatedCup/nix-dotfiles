530dc74cf6abe3c9246b6cd10c67f7d7b949699c
ChineseFriend101 <z.degidio159@gmail.com>
2024-05-23 14:44:17 -0400

    still working on stlix

--- nixos/core/themes.nix
+++ nixos/core/themes.nix
@@ -2,5 +2,9 @@
 
 {
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-  stylix.image = /home/zack/current-wallpaper/land.png;
+  stylix.image = pkgs.fetchurl {
+    url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+    sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+  };
+
 }
