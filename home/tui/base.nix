a214eabcfc60b46eddd746ee2c41e02dbeb03478
ChineseFriend101 <z.degidio159@gmail.com>
2024-05-26 23:00:29 -0400

    rearranged packages and got rid of package file

--- home/tui/base.nix
+++ home/tui/base.nix
@@ -33,4 +33,47 @@
   programs.tmux = {
     enable = true;
   };
+
+  programs.eza.enable = true;
+  programs.lazygit.enable = true;
+
+  home.packages = with pkgs; [
+
+    # Rice
+    pipes # Random pipes
+    fastfetch # Neofetch replacement but faster
+    pfetch # Minimal fetch
+    cowsay
+    cmatrix
+    cbonsai
+
+    fd # makes file indexing fire
+    fzf # A command-line fuzzy finder
+    ripgrep # recursively searches directories for a regex pattern
+    wev # Wayland even viewer (for keybinds)
+    gnumake
+
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    btop  # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+
+    # system tools
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+  ];
 }
