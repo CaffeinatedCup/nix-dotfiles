54d9179378cc08cd13ef3c6f0944dddaa00d9bf8
ChineseFriend101 <z.degidio159@gmail.com>
2024-08-23 17:24:01 -0400

    switch to rofi and start nvim conf

--- home/gui/browsers.nix
+++ home/gui/browsers.nix
@@ -14,6 +14,7 @@
 
   home.packages = with pkgs; [
     brave
+    gcc #must move
   ];
 
 }
--- home/gui/hyprland/hyprland.nix
+++ home/gui/hyprland/hyprland.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, nix-colors, ... }:
 
 {
   wayland.windowManager.hyprland = {
@@ -17,10 +17,30 @@
       source = ./conf;
       recursive = true;
     };
+
+    "/.config/rofi" = {
+      source = ./conf/rofi;
+      recursive = true;
+    };
   };
 
-  #application launcher themed by stylix
-  programs.fuzzel.enable = true;
+  #application launcher
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      colors = {
+        background = "0x${config.colorScheme.palette.base05}";
+        text = "0x${config.colorScheme.palette.base05}";
+        match = "0x${config.colorScheme.palette.base0C}";
+        selection = "0x${config.colorScheme.palette.base05}";
+        selection-match = "0x${config.colorScheme.palette.base05}";
+        selection-text = "0x${config.colorScheme.palette.base05}";
+        border = "0x${config.colorScheme.palette.base0E}";
+      };
+    };
+  };
+
+  #programs.rofi.enable = true;
 
   # Packages for hyprland
   home.packages = with pkgs; [
@@ -33,7 +53,6 @@
     hyprpicker #color picker
     mako #notification daemon
     swaybg
-    anyrun #test
-    walker #test
+    rofi-wayland
   ];
 }
