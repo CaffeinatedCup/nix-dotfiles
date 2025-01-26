238408b1dccff8a8e5604b504cea29ededc09fb5
ChineseFriend101 <z.degidio159@gmail.com>
2025-01-25 17:31:13 -0700

    removed nix-colors

--- home/gui/hyprland/hyprland.nix
+++ home/gui/hyprland/hyprland.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, nix-colors, ... }:
+{ config, pkgs, ... }:
 
 {
   wayland.windowManager.hyprland = {
@@ -22,25 +22,9 @@
       source = ./conf/rofi;
       recursive = true;
     };
+
   };
 
-  #application launcher
-  programs.fuzzel = {
-    enable = true;
-    settings = {
-      colors = {
-        background = "0x${config.colorScheme.palette.base05}";
-        text = "0x${config.colorScheme.palette.base05}";
-        match = "0x${config.colorScheme.palette.base0C}";
-        selection = "0x${config.colorScheme.palette.base05}";
-        selection-match = "0x${config.colorScheme.palette.base05}";
-        selection-text = "0x${config.colorScheme.palette.base05}";
-        border = "0x${config.colorScheme.palette.base0E}";
-      };
-    };
-  };
-
-  #programs.rofi.enable = true;
 
   # Packages for hyprland
   home.packages = with pkgs; [
