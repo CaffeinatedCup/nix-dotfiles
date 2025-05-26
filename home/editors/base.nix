4769d572dfe49aa6195c86675cf877a2e204667a
CaffeinatedCup <z.degidio159@gmail.com>
2025-05-26 15:11:35 -0600

    made the nixcats the default "nvim"

--- home/editors/base.nix
+++ home/editors/base.nix
@@ -3,10 +3,11 @@
 {
 
   # Neovim
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    };
+  #programs.neovim = {
+   # enable = true;
+   # defaultEditor = true;
+   # package = inputs.nvim-config.packages.${pkgs.system}.nvim;
+   # };
 
   # Emacs program and daemon
   programs.emacs.enable = true;
@@ -15,5 +16,6 @@
   home.packages = with pkgs; [
   rust-analyzer
   vscode-fhs
+  inputs.nvim-config.packages.${system}.nvim
   ];
 }
