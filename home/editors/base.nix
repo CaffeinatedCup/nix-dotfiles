dd5615c6705a007fbd5e4f0386f85dcf4d4d858b
CaffeinatedCup <z.degidio159@gmail.com>
2025-05-30 15:24:19 -0600

    cleanup

--- home/editors/base.nix
+++ home/editors/base.nix
@@ -1,13 +1,7 @@
 {config, pkgs, inputs, ... }:
 
 {
-
-  # Emacs program and daemon
-  programs.emacs.enable = true;
-  services.emacs.enable = true;
-
   home.packages = with pkgs; [
-  rust-analyzer
   vscode-fhs
   inputs.nvim-config.packages.${system}.nvim
   ];
--- home/gui/browsers.nix
+++ home/gui/browsers.nix
@@ -1,19 +1,11 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  # Browsers config packages
-
-  programs.firefox = {
-    enable = true;
-  };
-
-  programs.librewolf = {
-    enable = true;
-  };
-
 
   home.packages = with pkgs; [
     brave
+    firefox
+    librewolf
     gcc #must move
     inputs.zen-browser.packages."${system}".beta-unwrapped
   ];
