8fc972ad7d0adb410e47a394a889de7afbcba4f1
CaffeinatedCup <z.degidio159@gmail.com>
2025-10-01 17:30:35 -0600

    server test 1

--- hosts/liberator/default.nix
+++ hosts/liberator/default.nix
@@ -13,27 +13,10 @@
 
   networking.hostName = "liberator";
 
-  # Boot stuff specific to Darter Pro's Intel Arc
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelModules = [ "i915" ];
-
   hardware.system76.enableAll = true;
   
-  hardware = {
-    opengl.enable = true;
-    opengl.extraPackages = with pkgs; [
-    intel-compute-runtime 
-    vaapiIntel
-    ];
-  };
-  
-  environment.systemPackages = with pkgs; [ mesa ];
-
   # UEFI boot
   boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "nodev" ];
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }
 
