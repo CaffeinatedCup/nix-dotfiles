766fef1d64bba75e1b05600393ba697b018d0e46
ChineseFriend101 <z.degidio159@gmail.com>
2025-10-01 17:37:19 -0600

    readded some boot stuff for liberator

--- hosts/liberator/default.nix
+++ hosts/liberator/default.nix
@@ -17,6 +17,9 @@
   
   # UEFI boot
   boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }
 
