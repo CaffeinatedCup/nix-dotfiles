999d1162ad182e6f9de1835ada18a04e065cafaf
CaffeinatedCup <z.degidio159@gmail.com>
2025-10-22 19:31:54 -0600

    started desktop host and made liberator better

--- hosts/liberator/default.nix
+++ hosts/liberator/default.nix
@@ -27,6 +27,32 @@
     TERM = "xterm-256color";
   };
 
+  # Power saving for server
+  powerManagement = {
+  enable = true;
+  cpuFreqGovernor = "powersave";  # or "schedutil" for dynamic adjustment
+};
+
+services.tlp = {
+  enable = true;
+  settings = {
+    CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    WIFI_PWR_ON_AC = "on";
+    WIFI_PWR_ON_BAT = "on";
+    RUNTIME_PM_ON_AC = "auto";
+    RUNTIME_PM_ON_BAT = "auto";
+  };
+};
+
+  # Keep laptop on while lid is closed
+  services.logind = {
+  lidSwitch = "ignore";
+  lidSwitchDocked = "ignore";
+  lidSwitchExternalPower = "ignore";
+  };
+
+
   # UEFI boot
   boot.loader.grub.enable = true;
   boot.loader.grub.devices = [ "nodev" ];
