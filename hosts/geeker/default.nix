b387d2489c9208f7876d93819b7164ce4d2b54c8
CaffeinatedCup <z.degidio159@gmail.com>
2025-10-27 10:15:31 -0600

    added geeker config file

---
+++ hosts/geeker/default.nix
@@ -0,0 +1,21 @@
+## Geeker ##
+## AMD desktop with NixOS and Windows ##
+
+{ inputs, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../nixos/configuration.nix
+  ];
+
+  networking.hostName = "geeker";
+
+  # UEFI boot
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+}
+
