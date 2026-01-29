d66ddc847e5eb313af7acaf323d680ecefba91d2
CaffeinatedCup <z.degidio159@gmail.com>
2026-01-28 21:32:01 -0700

    no more quickshell config lol

--- home/files.nix
+++ home/files.nix
@@ -7,9 +7,8 @@
   "hypr/hypridle.conf".source = ./files/hypr/hypridle.conf;
   "hypr/hyprpaper.conf".source = ./files/hypr/hyprpaper.conf;
 
-  #"kitty/kitty.conf".source = ./files/kitty/tokyo-night.conf;
-
   "waybar/config".source = ./files/waybar/config;
+
   "waybar/style.css".source = ./files/waybar/style.css;
 
   "rofi/config.rasi".source = ./files/rofi/config.rasi;
