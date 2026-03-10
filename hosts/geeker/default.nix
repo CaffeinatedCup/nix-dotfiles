9f7a3d23489e5102da69e0e58b82236410b416d4
CaffeinatedCup <z.degidio159@gmail.com>
2026-03-10 11:10:47 -0600

    yup yup

--- hosts/geeker/default.nix
+++ hosts/geeker/default.nix
@@ -12,11 +12,25 @@
   networking.hostName = "geeker";
   services.xserver.videoDrivers = ["amdgpu"];
 
+  networking.networkmanager.wifi.backend = "wpa_supplicant";
+  networking.wireless.iwd.enable = false;
+  networking.networkmanager.wifi.powersave = false;
+
+  hardware.cpu.amd.updateMicrocode = true;
+
+  # disabled hardway encrytion for wifi card (twas getting stalls)
+  boot.extraModprobeConfig = ''
+  options ath9k nohwcrypt=1
+  '';
+
+
   # UEFI boot
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "nodev" ];
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.efiSupport = true;
+  boot.loader.systemd-boot.enable = true;
+  #boot.loader.grub.enable = true;
+  #boot.loader.grub.devices = [ "nodev" ];
+  #boot.loader.grub.useOSProber = true;
+  #boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
 }
 
