e71339f1c344910341efe73251b741ee9573fb8b
CaffeinatedCup <z.degidio159@gmail.com>
2026-04-29 17:38:53 -0600

    ltspice fixed, everything good

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Nix packages (stable)
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
     # Home manger
     home-manager.url = "github:nix-community/home-manager";
