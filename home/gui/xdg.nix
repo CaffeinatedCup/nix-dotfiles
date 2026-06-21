94d412996cf8bb224d8a20df8cf9dbb9ccc25911
CaffeinatedCup <z.degidio159@gmail.com>
2026-06-21 15:24:06 -0400

    sioyek now opens by default for pdf

--- home/gui/xdg.nix
+++ home/gui/xdg.nix
@@ -16,6 +16,28 @@
     dataHome = "${config.home.homeDirectory}/.local/share";
     stateHome = "${config.home.homeDirectory}/.local/state";
 
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/pdf" = "sioyek.desktop";
+
+        "text/html" = "zen.desktop";
+        "x-scheme-handler/http" = "zen.desktop";
+        "x-scheme-handler/https" = "zen.desktop";
+        "x-scheme-handler/about" = "brave-browser.desktop";
+        "x-scheme-handler/unknown" = "brave-browser.desktop";
+        "x-scheme-handler/chrome" = "zen.desktop";
+        "application/x-extension-htm" = "zen.desktop";
+        "application/x-extension-html" = "zen.desktop";
+        "application/x-extension-shtml" = "zen.desktop";
+        "application/xhtml+xml" = "zen.desktop";
+        "application/x-extension-xhtml" = "zen.desktop";
+        "application/x-extension-xht" = "zen.desktop";
+        "x-scheme-handler/mailspring" = "Mailspring.desktop";
+        "x-scheme-handler/claude-cli" = "claude-code-url-handler.desktop";
+      };
+    };
+
   #userDirs = {
    # enable = true;
    # createDirectories = true;
