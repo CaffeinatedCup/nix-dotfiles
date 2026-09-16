2d28ae954cab8c2ecec8de21a19aef3af6f7fc29
CaffeinatedCup <z.degidio159@gmail.com>
2026-09-15 20:37:03 -0600

    fixed sioyek file open

--- nixos/optional/desktop.nix
+++ nixos/optional/desktop.nix
@@ -7,6 +7,10 @@
   environment.sessionVariables = {
   XDG_CURRENT_DESKTOP = "niri";
   XDG_SESSION_TYPE = "wayland";
+  XDG_DATA_DIRS = [
+    "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+    "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+  ];
   };
 
     xdg.portal = {
